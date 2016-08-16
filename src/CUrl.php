cd1e925a4f3e9047fad076e6b50caa0b05297d8a
oangia <hqnhatdn@gmail.com>
2016-08-16 10:58:22 +0700

    remove old get method

--- src/CUrl.php
+++ src/CUrl.php
@@ -166,18 +166,4 @@ class CUrl {
 	private function resetHeaders() {
 		$this->headers = [];
 	}
-
-	public static function curl_get_file_contents( $url ) {
-		$c = curl_init();
-		curl_setopt( $c, CURLOPT_RETURNTRANSFER, 1 );
-		curl_setopt( $c, CURLOPT_URL, $url );
-		$contents = curl_exec( $c );
-		$err  = curl_getinfo( $c, CURLINFO_HTTP_CODE );
-		curl_close( $c );
-		if ( $contents ) {
-			return $contents;
-		} else {
-			return false;
-		}
-	}
 }
